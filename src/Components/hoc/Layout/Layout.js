64b7afbf8257c825cfbe62af5a9224269ee32743
bobbySandersJr <bobby.sanders.jr@outlook.com>
2019-03-10 00:14:35 -0600

    added styling for TopNavBar

--- src/Components/hoc/Layout/Layout.js
+++ src/Components/hoc/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import TopNavBar from '../../Navigation/TopNavBar/TopNavBar';
+
 class Layout extends Component {
   render() {
     return (
       <div>
-        HEADER
+        <TopNavBar />
         ROUTER
         FOOTER
       </div>
